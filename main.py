28e21d6b60d7f4c42c0877e01a1f73d59e43b384
Norman <illiand452@gmail.com>
2025-07-06 01:26:11 +0300

    add export to excel

--- main.py
+++ main.py
@@ -38,11 +38,13 @@
 from src.tle_parser import parser
 from src.read_tle import read_tle_file
 from src.processor import process_tle_records
+from src.exporter import export_to_excel
 
 
 def main():
     # названия выходных файлов и url'ы
-    name_output_tle_file = 'data/all_active_satellites.txt'
+    name_output_file = 'data/all_active_satellites.txt'
+    name_output_tle_file = name_output_file + '.txt'
     url = 'https://celestrak.org/NORAD/elements/geo.txt'
 
     # парсим и создаем файл с сырыми tle
@@ -54,6 +56,10 @@ def main():
     # вызываем функцию для обработки данных
     # 'XX.XXXXXX' - долгота (longitude), 'YY.YYYYYY' - широта (latitude)
     process_tle_records(tle_list, observer_lat=YY.YYYYYY, observer_lon=XX.XXXXXX)
+
+    # экспорт в Excel
+    name_output_xlsx_file = name_output_file + '.xlsx'
+    export_to_excel(output_file_path=name_output_xlsx_file)
 
 
 if __name__ == '__main__':
--- src/exporter.py
+++ src/exporter.py
@@ -1,1 +1,49 @@
 # Экспорт в Excel
+
+import pandas as pd
+from sqlalchemy import select
+from src.database import SessionLocal
+from src.models import Satellite, Calculation
+
+
+def export_to_excel(output_file_path: str):
+    session = SessionLocal()
+
+    # Словарь для хранения самых свежих расчётов по каждому спутнику
+    latest_calculations = {}
+
+    # Получаем все расчёты, отсортированные по времени
+    results = session.execute(
+        select(Calculation).order_by(Calculation.calculation_time.desc())
+    ).scalars().all()
+
+    for calc in results:
+        sat = calc.satellite
+        if sat.norad_id not in latest_calculations:
+            latest_calculations[sat.norad_id] = {
+                'ИСЗ': sat.name,
+                'Долгота': calc.longitude,
+                'Широта': calc.latitude,
+                'Азимут': calc.azimuth,
+                'Угол места': calc.elevation,
+                '№ NORAD': sat.norad_id,
+                'Межд.номер': sat.cospar_id,
+                'Наклонение': sat.inclination,
+                'Высота': calc.altitude,
+                'Дата создания TLE': sat.tle_created_at,
+                'Дата рассчетов': calc.calculation_time,
+            }
+
+    session.close()
+
+    # Преобразуем в DataFrame и сортируем по долготе
+    df = pd.DataFrame(latest_calculations.values())
+    df.sort_values(by='Долгота', inplace=True)
+
+    # Экспорт в Excel
+    df.to_excel(output_file_path, sheet_name='Спутники', index=False)
+    print(f'Данные экспортированы в {output_file_path}')
+
+
+if __name__ == '__main__':
+    export_to_excel('exported_satellites.xlsx')
